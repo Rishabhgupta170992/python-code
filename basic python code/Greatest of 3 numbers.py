cf6083ea484b84762f8c97172497ce78c98e44a7
Rishabhgupta170992 <115362720+Rishabhgupta170992@users.noreply.github.com>
2025-02-10 00:37:41 +0530

    Create Greatest of 3 numbers.py

---
+++ basic python code/Greatest of 3 numbers.py
@@ -0,0 +1,9 @@
+x =int(input("Enter the number:"))
+y =int(input("Enter the number:"))
+z =int(input("Enter the number:"))
+if (x>y and x>z):
+    print("greatest number is ", x)
+elif (y>z):
+    print("greatest number is ", y)
+else:
+    print("greatest number is ", z)
